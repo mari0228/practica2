b1f5f6594599086b0df399a15fc859e0d8014787
mari0228 <maricruzlopeznava1@gmail.com>
2024-10-09 08:23:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#f4f4f4', '#d1c4e9', '#ffccbc', '#c5cae9', '#b2dfdb'];
+    const colors = ['#e0f7fa', '#a5d6a7', '#ffcc80', '#b39ddb', '#80deea'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 });
